0ced5cab61dd45881e738ea2193a21eae57356ef
DesignsVerse <designsverseagency@gmail.com>
2025-02-14 12:33:10 +0530

    Update index.tsx

--- src/components/ExtraPage/index.tsx
+++ src/components/ExtraPage/index.tsx
@@ -30,7 +30,8 @@ const BaglamukhiSection = () => {
                 </li>
               ))}
             </ul>
-            <a href="https://api.whatsapp.com/send?phone=YOUR_PHONE_NUMBER" target="_blank" rel="noopener noreferrer">
+            <a href="https://api.whatsapp.com/send/?phone=919752165718&text=%E0%A4%A8%E0%A4%AE%E0%A4%B8%E0%A5%8D%E0%A4%A4%E0%A5%87%2C+%E0%A4%A8%E0%A4%82%E0%A4%A6%E0%A4%95%E0%A4%BF%E0%A4%B6%E0%A5%8B%E0%A4%B0+%E0%A4%9C%E0%A5%80%2C+%E0%A4%AE%E0%A5%81%E0%A4%9D%E0%A5%87+%E0%A4%AE%E0%A4%BE%E0%A4%81+%E0%A4%AC%E0%A4%97%E0%A4%B2%E0%A4%BE%E0%A4%AE%E0%A5%81%E0%A4%96%E0%A5%80+%E0%A4%AE%E0%A4%82%E0%A4%A6%E0%A4%BF%E0%A4%B0+%E0%A4%AE%E0%A5%87%E0%A4%82+%E0%A4%AA%E0%A5%82%E0%A4%9C%E0%A4%BE+%E0%A4%95%E0%A4%B0%E0%A4%B5%E0%A4%BE%E0%A4%A8%E0%A5%80+%E0%A4%B9%E0%A5%88%E0%A5%A4&
+" target="_blank" rel="noopener noreferrer">
               <button className="bg-[#d55f26] text-white font-semibold px-6 py-3 rounded-full shadow-md hover:bg-yellow-700 transition duration-300">
                 अभी बुक करें
               </button>
